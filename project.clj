41362366a7a5d3b7fd8fce7ef4be684788c968b5
jstaffans <johannes.staffans@gmail.com>
2015-05-25 20:51:22 +0200

    Placeholder Leiningen project file for Cursive

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject freq-words "0.1.0-SNAPSHOT"
+  :description "freq-words - project file for Cursive Clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[adzerk/boot-cljs      "0.0-2814-4"]
+                 [adzerk/boot-cljs-repl "0.1.9"]
+                 [adzerk/boot-reload    "0.2.4"]
+                 [pandeiro/boot-http    "0.6.1"]
+                 [reagent               "0.5.0"]])
+
