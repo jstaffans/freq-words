b4e1efe740ebba844a4c829fc08ed728a67cf52b
jstaffans <johannes.staffans@gmail.com>
2015-11-23 22:51:43 +0100

    word group selection

--- src/cljs/freq_words/app.cljs
+++ src/cljs/freq_words/app.cljs
@@ -1,28 +1,42 @@
 (ns freq-words.app
   (:require-macros [freq-words.macros :refer [defedn]])
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as r :refer [atom]]))
 
 (defedn words "words.edn")
 
+(def group-selection (r/atom nil))
+
 (defn word-group-component
   [words group-counter]
-  [:div {:class (str "col-md-4 word-group group-" (swap! group-counter inc)) :key (first words)}
-   (str (clojure.string/join ", " words) " …")])
+  (let [group-index (swap! group-counter inc)
+        class (str "col-md-4 word-group group-" group-index)]
+    [:div {:class    (str class
+                       (when-let [selection @group-selection]
+                         (if (= selection group-index)
+                           " selected"
+                           " not-selected")))
+           :key      (first words)
+           :on-click #(do
+                       (.log js/console group-index)
+                       (reset! group-selection group-index))}
+     (str (clojure.string/join ", " words) " …")]))
 
 (defn word-group-row-component
   [groups group-counter]
   [:div {:class "row" :key (-> groups flatten first)}
-   (for [group groups]
-     (word-group-component (take 3 group) group-counter))
+   (doall
+     (for [group groups]
+       (word-group-component (take 3 group) group-counter)))
    [:div {:style {:clear "left"}}]])
 
 (defn group-select-component []
   [:div
    [:h1 {:class "group-select"} "Välj grupp"]
-   (let [group-counter (atom 0)]
-     (for [groups (partition-all 3 (:groups words))]
-       (word-group-row-component groups group-counter)))])
+   (let [group-counter (r/atom 0)]
+     (doall
+       (for [groups (partition-all 3 (:groups words))]
+         (word-group-row-component groups group-counter))))])
 
 (defn init []
-  (reagent/render-component [group-select-component]
+  (r/render-component [group-select-component]
     (.getElementById js/document "container")))
