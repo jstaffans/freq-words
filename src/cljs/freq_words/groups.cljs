2f9ac08433b24c0d7811f1f2264ae5f9eb2bd2e7
jstaffans <johannes.staffans@gmail.com>
2015-12-05 16:24:57 +0100

    Style, transition updates

--- src/cljs/freq_words/groups.cljs
+++ src/cljs/freq_words/groups.cljs
@@ -18,22 +18,28 @@
   [:div {:class "controls"}
    [:div
     [:a {:class "btn btn-success"} "Kör!"]
-    [:a {:class "btn btn-link pull-right" :on-click #(reset! group-selection -1)} "Stäng"]]])
+    [:a {:class    "btn btn-link pull-right"
+         :on-click #(do
+                     (-> js/document .-activeElement .blur)
+                     (reset! group-selection -1))} "Stäng"]]])
 
 (defn word-group-component
   [{:keys [group-index group]}]
   (let [words (take 3 group)
-        class (str "col-md-4 panel word-group group-" group-index)
+        class (str "word-group group-" group-index)
         selected? (= @group-selection group-index)]
-    [:div {:class    (str class
-                       (when (>= @group-selection 0)
-                         (if selected?
-                           " selected"
-                           " not-selected")))
-           :key      group-index
-           :on-click #(when (not selected?) (reset! group-selection group-index))}
-     [:div {:class "preview"} (str (clojure.string/join ", " words) " …")]
-     (group-control-component group)]))
+
+    [:div {:class "col-md-4 word-col"}
+     [:div {:class     (str class
+                         (when (>= @group-selection 0)
+                           (if selected?
+                             " selected"
+                             " not-selected")))
+            :key       group-index
+            :tab-index group-index
+            :on-click  #(when (not selected?) (reset! group-selection group-index))}
+      [:div {:class "preview"} (str (clojure.string/join ", " words) " …")]
+      (group-control-component group)]]))
 
 (defn word-group-row-component
   [groups]
@@ -46,7 +52,7 @@
   [:div
    [:h1 {:class "group-select"} "Välj grupp"]
    (fore [group-row (->> (:groups words)
-                         (map-indexed (fn [i group] (assoc {} :group-index i :group group)))
+                         (map-indexed (fn [i group] (assoc {} :group-index (inc i) :group group)))
                          (partition-all 3))]
      (word-group-row-component group-row))])
 
